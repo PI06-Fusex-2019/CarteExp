cc0a6b6a52a418d0273d8472298d62a0d1675a8e
AlexisTPS <alexis0boulay@gmail.com>
2020-05-10 19:08:37 +0200

    AJout de condition pour ne pas saturé la FRAM et régulation de la fréquence d'execution

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 #include "radio.h"
 #include "define.h"
 
+//Initialisation des variables
+double ax, ay, az, gx, gy, gz, vz;
+int  adresse(1), temps(0);
+
 void setup() {
   //Initialisation des communications UART
   Serial_xbee.begin(9600);
@@ -15,17 +19,23 @@ void setup() {
   MPU_setup(); //accelerometre
   setupFRAM(); //FRAM
   setupPitot();
+  temps=micros();
 }
 
-//Initialisation des variables
-double ax, ay, az, gx, gy, gz, vz;
-int  adresse(1);
 
 void loop() {
+  if (micros()-temps>10)
+  {
+  if (adresse+8>262144)
+  {
+    while(1){}
+  }
   AccelerationReading(ax,ay,az);
   AngularAccelerationReading(gx,gy,gz);
   getspeed(vz);
   writeonFRAM(ax,ay,az,gx,gy,gz,vz,adresse);
   sendalldata(ax,ay,az,gx,gy,gz);
   adresse+=8;
+  temps=micros();
+  }
 }
